3e01559ab6c4975b934648e7ab3f7702127e7dde
TrsckySc <xieshaka5995@163.com>
2023-05-22 09:41:31 +0800

    :bug: fix: 字段为空异常处理

--- fast-common/src/main/java/com/j2eefast/common/core/listener/LicenseResponseBodyAdvice.java
+++ fast-common/src/main/java/com/j2eefast/common/core/listener/LicenseResponseBodyAdvice.java
@@ -1,5 +1,6 @@
 package com.j2eefast.common.core.listener;
 
+import cn.hutool.core.util.StrUtil;
 import cn.hutool.json.JSONArray;
 import cn.hutool.json.JSONUtil;
 import com.alibaba.fastjson.JSONObject;
@@ -11,6 +12,7 @@ import com.j2eefast.common.core.utils.JSON;
 import com.j2eefast.common.core.utils.PageUtil;
 import com.j2eefast.common.core.utils.ResponseData;
 import com.j2eefast.common.core.utils.ToolUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.MethodParameter;
 import org.springframework.http.HttpInputMessage;
@@ -26,6 +28,7 @@ import java.lang.reflect.Field;
 import java.lang.reflect.Type;
 import java.util.*;
 
+@Slf4j
 @ControllerAdvice
 public class LicenseResponseBodyAdvice implements ResponseBodyAdvice {
 
@@ -44,7 +47,9 @@ public class LicenseResponseBodyAdvice implements ResponseBodyAdvice {
             try{
                if( rd.get("data") instanceof PageUtil){
                    PageUtil page = (PageUtil) rd.get("data");
+                   //需要删除输出的字段
                    List<String> rmfields = new ArrayList<>();
+                   //需要转换的字段
                    List<Map<String,ConvertType>> conver = new ArrayList<>();
                    List<Map<String,String[]>> objList = new ArrayList<>();
                    if(page.getList().size() > 0){
@@ -135,11 +140,20 @@ public class LicenseResponseBodyAdvice implements ResponseBodyAdvice {
                                    ConvertType value = x.get(key);
                                    // 手机号码隐藏转换
                                    if(value.equals(ConvertType.PHONE)){
-                                       str.put(key,str.getStr(key).replaceAll("(\\d{3})\\d{4}(\\d{4})","$1****$2"));
+                                       if(!ToolUtil.isEmpty(str.getStr(key))){
+                                           str.put(key,str.getStr(key).replaceAll("(\\d{3})\\d{4}(\\d{4})","$1****$2"));
+                                       }else {
+                                           str.put(key, StrUtil.EMPTY);
+                                       }
+
                                    }
                                    // 邮箱隐藏转换
                                    if(value.equals(ConvertType.EMAIL)){
-                                       str.put(key,str.getStr(key).replaceAll("(\\w?)(\\w+)(\\w)(@\\w+\\.[a-z]+(\\.[a-z]+)?)", "$1****$3$4"));
+                                       if(!ToolUtil.isEmpty(str.getStr(key))){
+                                           str.put(key,str.getStr(key).replaceAll("(\\w?)(\\w+)(\\w)(@\\w+\\.[a-z]+(\\.[a-z]+)?)", "$1****$3$4"));
+                                       }else {
+                                           str.put(key, StrUtil.EMPTY);
+                                       }
                                    }
                                }
                            });
@@ -177,7 +191,7 @@ public class LicenseResponseBodyAdvice implements ResponseBodyAdvice {
                    return  jsonObject;
                }
             }catch (Exception e){
-               e.printStackTrace();
+                log.error("转换输出数据异常:",e);
             }
         }
         return body;
