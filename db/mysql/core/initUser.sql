b93b4d958f914740a927fc19ac1a2e7aa9947742
TrsckySc <xieshaka5995@163.com>
2023-12-21 02:25:04 +0800

    :art: 2.2.0 版本数据库文件

--- db/mysql/core/initUser.sql
+++ db/mysql/core/initUser.sql
@@ -2,6 +2,11 @@
  * 建立用户 - 数据库
  * @author zhouzhou
  * @date 2018-11-14 23:28
+ * 注意请设置检查你数据库配置文件 my.ini / my.cnf配置文件，在[mysqld]下添加编码配置
+ * #不区分大小写
+ * lower_case_table_names=1
+ * #设置sql_mode
+ * sql_mode=NO_ENGINE_SUBSTITUTION,STRICT_TRANS_TABLES
  ********************************************************************/
 ###############################创建Mysql数据库表#########################
 /** 注意: Mysql版本必须在5.7 以上**/
