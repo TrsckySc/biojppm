d7ffb80394f987eb821a01f05be1b5bd6606ee95
TrsckySc <xieshaka5995@163.com>
2024-02-02 11:29:44 +0800

    :arrow_up: 升级mybatisplus到最新版本3.4.1

--- fast-framework/src/main/java/com/j2eefast/framework/config/MybatisPlusConfig.java
+++ fast-framework/src/main/java/com/j2eefast/framework/config/MybatisPlusConfig.java
@@ -12,7 +12,10 @@ import com.j2eefast.framework.utils.UserUtils;
 import org.mybatis.spring.mapper.MapperScannerConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import com.baomidou.mybatisplus.autoconfigure.ConfigurationCustomizer;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 /**
@@ -26,20 +29,34 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 @EnableAspectJAutoProxy(proxyTargetClass = true, exposeProxy = true)
 public class MybatisPlusConfig {
 
+	/**
+     * 新的分页插件,一缓和二缓遵循mybatis的规则,需要设置 MybatisConfiguration#useDeprecatedExecutor = false 
+     * 避免缓存出现问题(该属性会在旧插件移除后一同移除)
+     * @author ZhouZhou
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        PaginationInnerInterceptor innerInterceptor =  new PaginationInnerInterceptor();
+        //设置最大分页数量
+        innerInterceptor.setMaxLimit(200L);
+        //对于单一数据库类型来说,都建议配置该值,避免每次分页都去抓取数据库类型
+        //innerInterceptor.setDbType(DbType.MYSQL);
+        interceptor.addInnerInterceptor(innerInterceptor);
+        return interceptor;
+    }
 
-//	@Bean
-//	public PerformanceInterceptor performanceInterceptor() {
-//		return new PerformanceInterceptor();
-//	}
+    @Bean
+    public ConfigurationCustomizer configurationCustomizer() {
+        return configuration -> configuration.setUseDeprecatedExecutor(false);
+    }
 
-	/*
-	 * 分页插件，自动识别数据库类型 多租户，请参考官网【插件扩展】
-	 */
-	@Bean
-	public PaginationInterceptor paginationInterceptor() {
-		return new PaginationInterceptor();
-	}
-
+    /**
+     * 自动填充
+     * @author ZhouZhou
+     * @date 2020-12-23 17:20
+     * @return
+     */
 	@Bean
 	public MybatisPulsMetaObjectHandler gunsMpFieldHandler() {
 		return new MybatisPulsMetaObjectHandler() {
