a3fd432d5316de3960e5a944aa189712d78728ab
TrsckySc <xieshaka5995@163.com>
2024-11-25 15:44:28 +0800

    :bookmark: :bookmark: :bookmark: Releasing / Version tags. 2.5.0

--- fast-common/src/main/java/com/j2eefast/common/core/config/DataSourceConfig.java
+++ fast-common/src/main/java/com/j2eefast/common/core/config/DataSourceConfig.java
@@ -31,7 +31,7 @@ import javax.sql.DataSource;
 @Configuration
 public class DataSourceConfig {
 
-	@Value("${fast.jta.enabled}")
+	@Value("#{ @environment['fast.jta.enabled'] ?: false }")
 	private boolean enabled;
 
 	/**
