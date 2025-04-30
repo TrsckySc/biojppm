4271cffbfdef648756c051899dad600bdecd5252
TrsckySc <xieshaka5995@163.com>
2025-04-30 14:40:50 +0800

    修改 freeMarker 模板引用 新增模板加载计算时间显示

--- fast-framework/src/main/java/com/j2eefast/framework/config/FreemarkerConfig.java
+++ fast-framework/src/main/java/com/j2eefast/framework/config/FreemarkerConfig.java
@@ -9,23 +9,65 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
+import com.j2eefast.common.core.page.JFreeMarkerViewResolver;
 import com.j2eefast.framework.utils.*;
 import com.jagregory.shiro.freemarker.ShiroTags;
 import freemarker.template.TemplateException;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.AutoConfigureOrder;
+import org.springframework.boot.autoconfigure.freemarker.FreeMarkerProperties;
+import org.springframework.boot.autoconfigure.task.TaskExecutionAutoConfiguration;
+import org.springframework.boot.autoconfigure.validation.ValidationAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.servlet.DispatcherServletAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
-import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver;
+import org.springframework.core.Ordered;
+import org.springframework.ui.freemarker.FreeMarkerConfigurationFactory;
+import org.springframework.web.servlet.view.freemarker.*;
 
 /**
  * Freemarker 模板引擎配置
  * @author zhouzhou
  */
 @Configuration
+@AutoConfigureOrder(Ordered.HIGHEST_PRECEDENCE + 11)
+@AutoConfigureAfter({ DispatcherServletAutoConfiguration.class, TaskExecutionAutoConfiguration.class,
+		ValidationAutoConfiguration.class })
 public class FreemarkerConfig {
+
+
+	private final FreeMarkerProperties properties;
+
+
+	protected final FreeMarkerProperties getProperties() {
+		return this.properties;
+	}
+
+	protected void applyProperties(FreeMarkerConfigurationFactory factory) {
+		factory.setTemplateLoaderPaths(this.properties.getTemplateLoaderPath());
+		factory.setPreferFileSystemAccess(this.properties.isPreferFileSystemAccess());
+		factory.setDefaultEncoding(this.properties.getCharsetName());
+		Properties settings = new Properties();
+		settings.put("recognize_standard_file_extensions", "true");
+		settings.putAll(this.properties.getSettings());
+		factory.setFreemarkerSettings(settings);
+	}
+
+	protected FreemarkerConfig(FreeMarkerProperties properties) {
+		this.properties = properties;
+	}
+
+	@Bean
+	JFreeMarkerViewResolver jfreeMarkerViewResolver() {
+		JFreeMarkerViewResolver resolver = new JFreeMarkerViewResolver();
+		getProperties().applyToMvcViewResolver(resolver);
+		return resolver;
+	}
 	
 	@Bean
-	public FreeMarkerConfigurer freeMarkerConfigurer(SysConfig sysconfig,
+	public FreeMarkerConfigurer freeMarkerConfigurer(
+													 Tools tools,
+													 SysConfig sysconfig,
 													 DictConfig dict,
 													 PermissionConfig permission)
 			 										 throws IOException, TemplateException {
@@ -35,6 +77,7 @@ public class FreemarkerConfig {
 		variables.put("config", sysconfig);
 		variables.put("dict",dict);
 		variables.put("permission",permission);
+		variables.put("tools",tools);
 		configurer.setFreemarkerVariables(variables);
 		Properties settings = new Properties();
 		settings.setProperty("default_encoding", "utf-8");
